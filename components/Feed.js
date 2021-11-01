8017eb0b0a1daadd71e58134a48b01a5e7df2fab
Gyunbie <gyunbie@gmail.com>
2021-11-01 17:01:08 +0300

    Add home page, fix header for mobile

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,10 @@
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
 import Suggestions from "./Suggestions";
+import Image from "next/image";
+import { LoginIcon } from "@heroicons/react/outline";
 
 function Feed() {
   const { data: session } = useSession();
@@ -10,7 +12,7 @@ function Feed() {
   return (
     <main
       className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${
-        !session && "!grid-cols-1 !max-w-3xl"
+        !session && "!grid-cols-1 !max-w-6xl"
       }`}
     >
       {session ? (
@@ -34,7 +36,25 @@ function Feed() {
           </section>
         </>
       ) : (
-        <h1>PLEASE LOG IN</h1>
+        <div className="flex justify-between items-center mx-auto">
+          <div className="flex flex-col items-center">
+            <img src="/images/instagram-landing-phones.png" alt="" />
+            <img
+              src="https://links.papareact.com/ocw"
+              alt=""
+              className="w-96"
+            />
+
+            <button
+              onClick={signIn}
+              className="flex items-center text-white text-xl font-bold bg-blue-500 rounded-lg px-2 py-1.5 hover:bg-blue-400 duration-150"
+            >
+              <p className="flex">
+                <LoginIcon className="h-8 w-8" /> Sign In with Google
+              </p>
+            </button>
+          </div>
+        </div>
       )}
     </main>
   );
--- components/Header.js
+++ components/Header.js
@@ -19,80 +19,87 @@ function Header() {
   const router = useRouter();
 
   return (
-    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
-      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
-        {/* Left - Branding */}
-        <div
-          onClick={() => router.push("/")}
-          className="relative hidden lg:inline-grid w-24 cursor-pointer"
-        >
-          <Image
-            src="https://links.papareact.com/ocw"
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-
-        <div
-          className="relative w-10 lg:hidden flex-shrink-0"
-          onClick={() => router.push("/")}
-        >
-          <Image
-            src="https://links.papareact.com/jjm"
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-
-        {/* Middle - Search input field */}
-        <div className="max-w-xs">
-          <div className="relative mt-1 p-3 rounded-md">
-            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-              <SearchIcon className="h-5 w-5 text-gray-500" />
+    <>
+      {session && (
+        <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+          <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
+            {/* Left - Branding */}
+            <div
+              onClick={() => router.push("/")}
+              className="relative hidden lg:inline-grid w-24 cursor-pointer"
+            >
+              <Image
+                src="https://links.papareact.com/ocw"
+                layout="fill"
+                objectFit="contain"
+              />
             </div>
 
-            <input
-              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black rounded-md"
-              type="text"
-              placeholder="Search"
-            />
-          </div>
-        </div>
+            <div
+              className="relative p-5 m-3 w-10 lg:hidden flex-shrink-0"
+              onClick={() => router.push("/")}
+            >
+              <Image
+                src="https://links.papareact.com/jjm"
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
 
-        {/* Right */}
-        <div className="flex items-center justify-end space-x-4">
-          <HomeIcon onClick={() => router.push("/")} className="button-nav" />
-          <MenuIcon className="h-6 md:hidden cursor-pointer" />
-
-          {session ? (
-            <>
-              <div className="relative button-nav">
-                <PaperAirplaneIcon className="button-nav rotate-90" />
-                <div className="absolute -top-1 -right-2 text-xs h-5 w-5 bg-red-600 rounded-full flex items-center justify-center opacity-90 animate-pulse text-white">
-                  3
+            {/* Middle - Search input field */}
+            <div className="max-w-xs">
+              <div className="hidden md:inline-flex relative mt-1 p-3 rounded-md">
+                <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                  <SearchIcon className="h-5 w-5 text-gray-500" />
                 </div>
-              </div>
 
-              <PlusCircleIcon
-                onClick={() => setOpen(true)}
+                <input
+                  className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black"
+                  type="text"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+
+            {/* Right */}
+            <div className="flex items-center justify-end space-x-4">
+              <HomeIcon
+                onClick={() => router.push("/")}
                 className="button-nav"
               />
-              <UserGroupIcon className="button-nav" />
-              <HeartIcon className="button-nav" />
+              <MenuIcon className="button-nav" />
 
-              <img
-                src={session?.user?.image}
-                alt="Profile photo"
-                className="h-8 w-8 rounded-full cursor-pointer"
-                onClick={signOut}
-              />
-            </>
-          ) : (
-            <button onClick={signIn}>Sign In</button>
-          )}
+              {session ? (
+                <>
+                  <div className="relative button-nav">
+                    <PaperAirplaneIcon className="button-nav rotate-90" />
+                    <div className="absolute -top-1 -right-2 text-xs h-5 w-5 bg-red-600 rounded-full flex items-center justify-center opacity-90 animate-pulse text-white">
+                      3
+                    </div>
+                  </div>
+
+                  <PlusCircleIcon
+                    onClick={() => setOpen(true)}
+                    className="button-nav"
+                  />
+                  <UserGroupIcon className="button-nav" />
+                  <HeartIcon className="button-nav" />
+
+                  <img
+                    src={session?.user?.image}
+                    alt="Profile photo"
+                    className="h-8 w-8 rounded-full cursor-pointer"
+                    onClick={signOut}
+                  />
+                </>
+              ) : (
+                <button onClick={signIn}>Sign In</button>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
