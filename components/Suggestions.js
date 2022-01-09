1b65294f88c69f5e1061f4395d8e3a8d10802dd4
gyunbie <gyunbie@gmail.com>
2022-01-09 15:28:41 +0300

    Fix suggestions avatar

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -27,13 +27,15 @@ function Suggestions() {
         >
           <img
             className="w-10 h-10 rounded-full border p-[2px]"
-            src={profile.avatar}
+            src="https://cdn.fakercloud.com/avatars/guischmitt_128.jpg"
             alt=""
           />
 
           <div className="flex-1 ml-4">
             <h2 className="font-semibold text-sm">{profile.username}</h2>
-            <h3 className="text-xs text-gray-400">Works at {profile.company.name}</h3>
+            <h3 className="text-xs text-gray-400">
+              Works at {profile.company.name}
+            </h3>
           </div>
 
           <button className="text-blue-400 text-xs font-bold">Follow</button>
