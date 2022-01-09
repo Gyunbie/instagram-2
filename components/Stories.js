1f3cbeda7c89ee6ccc21b4affed09a5d52d5ffe0
gyunbie <gyunbie@gmail.com>
2022-01-09 15:23:03 +0300

    Fix profile image

--- components/Stories.js
+++ components/Stories.js
@@ -19,7 +19,7 @@ function Stories() {
       {suggestions.map((profile) => (
         <Story
           key={profile.id}
-          img={faker.image.avatar()}
+          img="https://cdn2.greatfon.com/view?q=4IWYxUzN9QWaz91Yu9lJGRTN4ETRxYTPl9mJRpmbQFFUJNWaYhkSUV1aVRWRVpGROljbtoGMjdWc4FkeVJ3cHhnZ4VnMplDVB9FMw0DavZCNtcjYjNWLy4iasd0YmZ1RiBnWyIWeCNDW6Z1MiRHbuJmd1cVW9kXZr9VZoNWYj91ZpZCNtcTPiN2YmEUQBFkQwQkRZFWRB1TbkVmJmR1YZ9VOYF0bzlEVpNEO3ZlVq1zYo92Xj52XmETP0F2YfNmbfZCdl5mLuR2YiZmLh5mZuQTLxITahNmZu0WYydWY0Nnbp1Ddo91Yu91PnBnau42XycjM2IzM0MzN5gTN5YDM2QDNy8VOxUjM4gjMwEzNwcTN0MzX4EjM0gDO0QzL5ETL1gDOy4SM1Q3L29Cdl5mLuR2YiZmLh5mZuQTLxITahNmZu0WYydWY0Nnbp9yL6MHc0RHa"
           username={profile.username}
         />
       ))}
