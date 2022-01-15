9e5ef51c512b750602c4ee3a293ff85fb60a1c2f
Gyunbie <47539319+Gyunbie@users.noreply.github.com>
2022-01-15 15:17:22 +0300

    Update Suggestions.js

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -27,7 +27,7 @@ function Suggestions() {
         >
           <img
             className="w-10 h-10 rounded-full border p-[2px]"
-            src="https://cdn2.greatfon.com/view?q=4IWYxUzN9QWaz91Yu9lJGRTN4ETRxYTPl9mJRpmbQFFUJNWaYhkSUV1aVRWRVpGROljbtoGMjdWc4FkeVJ3cHhnZ4VnMplDVB9FMw0DavZCNtcjYjNWLy4iasd0YmZ1RiBnWyIWeCNDW6Z1MiRHbuJmd1cVW9kXZr9VZoNWYj91ZpZCNtcTPiN2YmEUQBFkQwQkRZFWRB1TbkVmJmR1YZ9VOYF0bzlEVpNEO3ZlVq1zYo92Xj52XmETP0F2YfNmbfZCdl5mLuR2YiZmLh5mZuQTLxITahNmZu0WYydWY0Nnbp1Ddo91Yu91PnBnau42XycjM2IzM0MzN5gTN5YDM2QDNy8VOxUjM4gjMwEzNwcTN0MzX4EjM0gDO0QzL5ETL1gDOy4SM1Q3L29Cdl5mLuR2YiZmLh5mZuQTLxITahNmZu0WYydWY0Nnbp9yL6MHc0RHa"
+            src="https://ssl.ulximg.com/static/images/default_avatar.jpg"
             alt=""
           />
 
